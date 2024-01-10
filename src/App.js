1f312f3c4a81bb79de272f5c625aff5ecc21b08d
Marcos Zorzi Rosa <marcoszorzi@gmail.com>
2024-01-10 18:47:39 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './style'
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+          Hero
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          Click to Action
+          Footer
+      </div>
+    </div>
+  </div>
+)
+
+export default App
+
+
+/**
+ * Video stopped at 6:54:22
+ * https://www.youtube.com/watch?v=F627pKNUCVQ&t=499s
+ */
