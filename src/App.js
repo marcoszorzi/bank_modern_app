b87aec9413efa6d3416d5f278cac31b2c3b57655
Marcos Zorzi Rosa <marcoszorzi@gmail.com>
2024-01-16 17:06:51 -0500

    Components base

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import styles from './style'
 
+import {Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          Click to Action
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
       </div>
     </div>
   </div>
@@ -34,6 +36,6 @@ export default App
 
 
 /**
- * Video stopped at 6:54:22
+ * Video stopped at 7:16:13
  * https://www.youtube.com/watch?v=F627pKNUCVQ&t=499s
  */
